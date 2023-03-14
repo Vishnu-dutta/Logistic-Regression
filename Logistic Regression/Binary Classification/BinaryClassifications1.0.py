849f51845f7b78f6ba6e03fcfd34065f7734975b
Vishnu-dutta <111376662+Vishnu-dutta@users.noreply.github.com>
2023-03-14 21:57:53 +0530

    Fixing

--- Logistic Regression/Binary Classification/BinaryClassifications1.0.py
+++ Logistic Regression/Binary Classification/BinaryClassifications1.0.py
@@ -10,17 +10,18 @@ from sklearn import preprocessing
 import numpy as np
 
 df = pd.read_csv("F:\\Setups\\py-master\\py-master\\ML\\7_logistic_reg\\Exercise\\HR_comma_sep.csv")
-ohe = ColumnTransformer([("one_hot_encoding", OneHotEncoder(), [3])], remainder='passthrough')
 le = LabelEncoder()
+ohe = ColumnTransformer([("one_hot_encoding", OneHotEncoder(), [2])], remainder='passthrough')
 
 df["salary"] = le.fit_transform(df["salary"])
-X = df[["satisfaction_level", "average_montly_hours", "promotion_last_5years", "salary"]].values
+X = df[["satisfaction_level", "average_montly_hours", "salary", "promotion_last_5years" ]].values
 X = np.array(ohe.fit_transform(X), dtype='float')
 
-X = X[:,:2]
+X = X[:, 1:]
 y = df["left"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
 reg = LogisticRegression()
 reg.fit(X_train, y_train)
 print(reg.score(X_test, y_test))
+print(reg.predict([[1,0,0.58,186,0]]))
